ab0793e5d9e2e6483fb51d1382cf83eabe97d9f0
rern <rernrern@gmail.com>
2026-07-18 16:23:34 +0700

    u

--- compile.sh
+++ compile.sh
@@ -4,34 +4,28 @@
 
 SECONDS=0
 
-if [[ -e /boot/kernel8.img ]]; then
-	arch=aarch64
-elif [[ -e /boot/kernel7.img ]]; then
-	arch=armv7h
-else
+if [[ -e /boot/kernel.img ]]; then
 	arch=armv6h     #fix sysroot path
+	file_bin=_status
 	opt='-Wno-psabi -idirafter /usr/include'
          #suppress warnings
+else
+	[[ -e /boot/kernel7.img ]] && arch=armv7h || arch=aarch64
+	file_bin=status
 fi
 
 cat << EOF
-g++ -O2 $opt _status.cpp -o status \
+g++ -O2 $opt _status.cpp -o $file_bin \
     $( pkg-config --cflags --libs alsa dbus-1 libcurl libmpdclient libupnpp taglib )
 ...
 EOF
 
     #strip
-g++ -O2 $opt _status.cpp -o status \
+g++ -O2 $opt _status.cpp -o $file_bin \
     $( pkg-config --cflags --libs alsa dbus-1 libcurl libmpdclient libupnpp taglib )
 
-if [[ $arch == armv6h ]]; then
-	mv /srv/http/bash/_status{,.bak}
-	cp -f status /srv/http/bash/_status
-	cp -f status{,.$arch}
-else
-	mv /srv/http/bash/status{,.bak}
-	cp -f status /srv/http/bash
-	cp -f status{,.$arch}
-fi
+mv /srv/http/bash/$file_bin{,.bak}
+cp -f $file_bin /srv/http/bash
+mv $file_bin status.$arch
 
 echo Compile duration: $SECONDS seconds
