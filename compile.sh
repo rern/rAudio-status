391e0e80b661aa05db983207cf044fcb4c62a4b7
rern <rernrern@gmail.com>
2026-07-19 08:38:19 +0700

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -5,20 +5,28 @@
 SECONDS=0
 
 if [[ -e /boot/kernel.img ]]; then
-	arch=armv6h     #fix sysroot path
+	arch=armv6h
 	file_bin=_status
-	opt='-Wno-psabi -idirafter /usr/include'
-         #suppress warnings
+	opt='
+-Wno-psabi
+-idirafter /usr/include
+'
 else
 	[[ -e /boot/kernel7.img ]] && arch=armv7h || arch=aarch64
 	file_bin=status
 fi
      #strip
-opt="-O2 $opt _status.cpp -o $file_bin $( pkg-config --cflags --libs alsa dbus-1 libcurl libmpdclient libupnpp taglib )"
-echo "
-g++ $opt
-...
+opt="
+-O2
+$opt
+_status.cpp
+-o $file_bin
+$( pkg-config --cflags --libs alsa dbus-1 libcurl libmpdclient libupnpp taglib )
 "
+echo 
+echo g++ $opt
+echo ...
+
 g++ $opt
 
 mv /srv/http/bash/$file_bin{,.bak}
